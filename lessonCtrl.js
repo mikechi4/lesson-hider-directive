fd2111b31e76aaa94258739e418f9c854ea53c99
Mike <mike.chi4@gmail.com>
2016-10-04 15:24:19 -0600

    finished up to step 4

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -1,4 +1,7 @@
 angular.module('directivePractice')
   .controller('mainCtrl', function($scope){
-    
+    $scope.lessons = ['Services', 'Routing', 'Directives', 'Review',
+     'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+
+    $scope.test = 'Two way data binding!';
   });
--- lessonHider.js
+++ lessonHider.js
@@ -1,7 +1,17 @@
 angular.module('directivePractice')
   .directive('lessonHider', function(){
+
     return{
-      templateUrl: 'lessonHider.html'
+      templateUrl: 'lessonHider.html',
+      restrict: 'E',
+      scope: {
+        lesson: '='
+      },
+      link: function(scope, element, attributes){
+        console.log(scope);
+        console.log(element);
+        console.log(attributes);
+      }
     }
   })
 ;
